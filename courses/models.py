d18d43b9dd3eb763420d441757c2dfb09b831e62
Sreeshma Tharammal <sreeshmajeenus@gmail.com>
2024-05-28 20:10:02 +0000

    Create courses model

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from users.models import Student
 
 # Create your models here.
+class Course(models.Model):
+    """
+    Model for course
+    """
+    name = models.CharField(max_length = 100)
+    description = models.TextField()
+
+class Enrollment(models.Model):
+    """
+    Model for enrollment
+    """
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, null = True, blank = True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
