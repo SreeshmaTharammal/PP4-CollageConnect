e78c4aee2a1c87503c1c034282444e1ebc157635
Sreeshma Tharammal <sreeshmajeenus@gmail.com>
2024-06-24 21:31:35 +0000

    Change natinality to combo box to show all countries

--- college_connect/settings.py
+++ college_connect/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'subjects',
     'attendance',
     'home',
+    'django_countries',
 ]
 
 MIDDLEWARE = [
--- users/models.py
+++ users/models.py
@@ -1,15 +1,22 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from django_countries import countries
+
+NATIONALITY_CHOICES = [(code, name) for code, name in list(countries)]
 
 class User(AbstractUser):
+    address = models.CharField(max_length=255, null=True, blank=True)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    emergency_contact_number = models.CharField(max_length=20, null=True, blank=True)
+    nationality = models.CharField(max_length=2, choices=NATIONALITY_CHOICES, blank=True, null=True)
+
     is_student = models.BooleanField(default=False)
     is_instructor = models.BooleanField(default=False)
     is_guest = models.BooleanField(default=True)
-    address = models.TextField(blank=True, null=True)
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
-    emergency_contact_number = models.CharField(max_length=15, blank=True, null=True)
-    nationality = models.CharField(max_length=50, blank=True, null=True)
 
+    def __str__(self):
+        return self.username
 
     @property
     def is_admin(self):
